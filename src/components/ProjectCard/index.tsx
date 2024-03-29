1ff7e62299e44bd86c5fd76208e4a472e06f451b
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-03-29 23:19:13 +0700

    refactor: change project data with props

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,8 +1,16 @@
+import React from "react";
 import "./index.css"
-import helenaLogo from "../../assets/images/helena-logo-white.svg"
+
 import rightArrow from "../../assets/images/right-arrow 1.svg"
 
-const ProjectCard = () => {
+interface ProjectsData {
+  name: string,
+  description: string,
+  imgSources: string,
+  linkProjects: string
+}
+
+const ProjectCard: React.FC<ProjectsData> = ({name, description, imgSources, linkProjects}) => {
   return (
     <>
       <div className="flex flex-col items-start md:justify-evenly justify-center lg:gap-14 md:gap-11 gap-10 md:min-h-80 md:w-944">
@@ -10,14 +18,14 @@ const ProjectCard = () => {
         <div className="md:grid lg:grid-cols-4 md:grid-cols-3 md:gap-9 flex flex-col gap-10">
             <div className="w-52 h-56 bg-white shadow-custom">
                 <div className="flex items-center justify-center bg-custom w-52 h-28">
-                    <img src={helenaLogo} className="w-4/5 h-auto" alt="helena logo" />
+                    <img src={imgSources} className="w-4/5 h-auto" alt="helena logo" />
                 </div>
                 <div className="flex flex-col items-center justify-center gap-5 w-52 h-28 bg-white">
                     <div className="flex flex-col gap-1">
-                        <h1 className="text-lg font-bold">Helena</h1>
-                        <p className="text-sm">Health tracking web app</p>
+                        <h1 className="text-lg font-bold">{name}</h1>
+                        <p className="text-sm">{description}</p>
                     </div>
-                    <a href="" className="flex self-end mr-5">
+                    <a href={linkProjects} className="flex self-end mr-5">
                         <img src={rightArrow} className="h-4 w-4" alt="right arrow" />
                     </a>
                 </div>
--- src/containers/ProjectSection/index.tsx
+++ src/containers/ProjectSection/index.tsx
@@ -1,11 +1,12 @@
 import { ProjectCard } from "../../components";
 import "./index.css"
+import helenaLogo from "../../assets/images/helena-logo-white.svg"
 
 const ProjectSection = () => {
   return (
     <>
       <div id="projects" className="flex items-center justify-center min-h-96 mt-14">
-        <ProjectCard/>
+        <ProjectCard name={"Helena"} description={"Health Tracker Web App"} imgSources={helenaLogo} linkProjects={""}/>
       </div>
     </>
   );
