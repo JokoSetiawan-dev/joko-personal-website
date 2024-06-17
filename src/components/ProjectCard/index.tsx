4808d2adb9fcffc1ed05aacc14b30702431120d0
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-06-18 00:21:45 +0700

    feat: add project details

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,37 +1,54 @@
 import React from "react";
-import "./index.css"
-
-import rightArrow from "../../assets/images/right-arrow 1.svg"
+import "./index.css";
+import github from "../../assets/images/github-logo.png";
+import production from "../../assets/images/world-wide-web.png"
 
 interface ProjectsData {
-  name: string,
-  description: string,
-  imgSources: string,
-  linkProjects: string
+  description: string;
+  imgSources: string;
+  linkGithub: string;
+  techStack: string;
+  linkWeb: string;
 }
 
-const ProjectCard: React.FC<ProjectsData> = ({name, description, imgSources, linkProjects}) => {
+const ProjectCard: React.FC<ProjectsData> = ({
+  description,
+  imgSources,
+  linkGithub,
+  techStack,
+  linkWeb,
+}) => {
   return (
     <>
       <div className="flex flex-col items-start md:justify-evenly justify-center lg:gap-14 md:gap-11 gap-10 md:min-h-80 md:w-944">
-        <h1 className="text-xl font-bold flex self-center">Works</h1>
-        <div className="md:grid lg:grid-cols-4 md:grid-cols-3 md:gap-9 flex flex-col gap-10">
-            <div className="w-52 h-56 bg-white shadow-custom">
-                <div className="flex items-center justify-center bg-custom w-52 h-28">
-                    <img src={imgSources} className="w-4/5 h-auto" alt="helena logo" />
-                </div>
-                <div className="flex flex-col items-center justify-center gap-5 w-52 h-28 bg-white">
-                    <div className="flex flex-col gap-1">
-                        <h1 className="text-lg font-bold">{name}</h1>
-                        <p className="text-sm">{description}</p>
-                    </div>
-                    <a href={linkProjects} className="flex self-end mr-5">
-                        <img src={rightArrow} className="h-4 w-4" alt="right arrow" />
-                    </a>
-                </div>
+        <div >
+          <div className="w-[320px] h-[400px] bg-white shadow-custom">
+            <div className="flex items-center justify-center bg-custom w-full h-36">
+              <img
+                src={imgSources}
+                className="w-4/5 h-auto"
+                alt="helena logo"
+              />
             </div>
+            <div className="flex flex-col items-start py-3 px-4 justify-center gap-5 w-full h-auto bg-white">
+              <div className="flex flex-col gap-1">
+                <p className="text-ssm mt-2">{description}</p>
+              </div>
+              <div>
+                <h1 className="text-sm">Tech Stack :</h1>
+                <p className="text-ssm mt-1">{techStack}</p>
+              </div>
+              <div className="flex gap-5">
+                <a href={linkGithub}>
+                  <img src={github} className="h-7 w-7" alt="right arrow" />
+                </a>
+                <a href={linkWeb}>
+                  <img src={production} className="h-7 w-7" alt="right arrow" />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-        <a href="" className="flex self-center bg-custom-skyblue font-bold text-white py-2 px-4">See More</a>
       </div>
     </>
   );
--- src/containers/ProjectSection/index.tsx
+++ src/containers/ProjectSection/index.tsx
@@ -1,12 +1,52 @@
 import { ProjectCard } from "../../components";
-import "./index.css"
-import helenaLogo from "../../assets/images/helena-logo-white.svg"
+import "./index.css";
+import helenaLogo from "../../assets/images/helena-logo-white.svg";
+import bankz from "../../assets/images/LOGO3-01.webp"
+import unisky from "../../assets/images/unisky-logo.svg"
 
 const ProjectSection = () => {
   return (
     <>
-      <div id="projects" className="flex items-center justify-center min-h-96 mt-14">
-        <ProjectCard name={"Helena"} description={"Health Tracker Web App"} imgSources={helenaLogo} linkProjects={""}/>
+      <div
+        id="projects"
+        className="flex flex-col items-center justify-center w-full min-h-96 mt-14"
+      >
+        <h1 className="text-xl font-bold flex self-center mb-10">Works</h1>
+        <div className="md:grid lg:grid-cols-3 md:grid-cols-3 md:gap-9 flex flex-col gap-10">
+          <ProjectCard
+            description={
+              "Unisky Weather App delivers accurate daily and weekly forecasts with an intuitive, user-friendly interface. Stay prepared for any weather conditions."
+            }
+            imgSources={unisky}
+            linkGithub={"https://github.com/JokoSetiawan-dev/unisky-weather-app"}
+            linkWeb={"https://unisky-weather-app.vercel.app/"}
+            techStack={
+              "NextJs, Typesript, Shadcn, Tailwind CSS"
+            }
+          />
+          <ProjectCard
+            description={
+              "Helena: Health Tracker is a web app that lets users monitor and manage health with real-time visualization and personalized insights, offering an engaging, user-friendly experience."
+            }
+            imgSources={helenaLogo}
+            linkGithub={"https://github.com/Helena-health-tracker-assistant/Helena-client"}
+            linkWeb={"https://helena-health-tracker.web.app/home"}
+            techStack={
+              "NodeJS, ReactJs, MaterialUI, Framer Motion, Recharts, Google Maps API, Formik, Yup"
+            }
+          />
+          <ProjectCard
+            description={
+              "Bank of Z's web profile, crafted using HTML, JavaScript, and CSS, provides a secure and user-friendly platform for online banking."
+            }
+            imgSources={bankz}
+            linkGithub={"https://github.com/RevoU-FSSE-2/week-5-JokoSetiawan-dev"}
+            linkWeb={"https://investreeassw4.site/"}
+            techStack={
+              "JavaScript, HTML, CSS"
+            }
+          />
+        </div>
       </div>
     </>
   );
